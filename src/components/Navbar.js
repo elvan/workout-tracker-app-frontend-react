5b48765a47ab68fb9e3cd8100a5beb6030ff288f
Elvan Hidayat <elvanhd@gmail.com>
2023-02-28 03:04:53 +0700

    add screenshots

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -14,7 +14,7 @@ const Navbar = () => {
     <header>
       <div className='container'>
         <Link to='/'>
-          <h1>Workout Buddy</h1>
+          <h1>Workout Tracker</h1>
         </Link>
         <nav>
           {user && (
